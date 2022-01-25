1863f7c5ff2bc84bf907192626b0ad4f48a6114f
dimitryKamga <dimitrykamga@gmail.com>
2022-01-24 23:36:15 -0500

    Create run.sh and added report txt

---
+++ TP1/run.sh
@@ -0,0 +1,2 @@
+python -m coverage run -m --source=. --omit=main.py,renege.py,text_cleaner.py,test_*.py,venv/* --branch unittest test_crud.py test_email_analyzer.py test_vocabulary_creator.py
+python -m coverage report | tee coverage_report.txt
