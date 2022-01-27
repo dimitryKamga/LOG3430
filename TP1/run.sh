0080610925d4c8a13dd6bcca899ae9482f296ba2
dimitryKamga <dimitrykamga@gmail.com>
2022-01-27 17:08:40 -0500

    Add tp1 pdf and correct script

--- TP1/run.sh
+++ TP1/run.sh
@@ -1,2 +1,3 @@
 python -m coverage run -m --source=. --omit=main.py,renege.py,text_cleaner.py,test_*.py,venv/* --branch unittest test_crud.py test_email_analyzer.py test_vocabulary_creator.py
 python -m coverage report | tee coverage_report.txt
+coverage html
