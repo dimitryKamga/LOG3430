9f60dd78246ddbd65def6d3343d153b499784fb5
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2022-04-05 13:04:08 -0400

    adding tests to test_main

--- TP5/src/test_main.py
+++ TP5/src/test_main.py
@@ -4,8 +4,6 @@ from main import evaluate
 from vocabulary_creator import VocabularyCreator
 from renege import RENEGE
 
-
-
 class TestMain(unittest.TestCase):
     @classmethod
     def setUpClass(self):
@@ -20,6 +18,19 @@ class TestMain(unittest.TestCase):
             renege = RENEGE(trainFile)
             renege.classify_emails()
 
+    
+    def test_clean_emails_train_set(self):
+        print("----------test_clean_train_set----------")
+        self.prepareVocab("train_clean.json")
+        accuracy=evaluate("test300.json")
+        self.assertLessEqual(abs(self.initialAccuracyTest-accuracy),0.03)
+        
+    def test_clean_emails_test_set(self):
+        print("----------test_clean_test_set----------")
+        self.prepareVocab("train700.json")
+        accuracy=evaluate("test_clean.json")
+        self.assertLessEqual(abs(self.initialAccuracyTest-accuracy),0.03)
+    
     def test_shuffled_words_train_set(self):
         print("----------test_shuffled_words_train_set----------")
         self.prepareVocab("train_shuffle.json")
@@ -42,6 +53,18 @@ class TestMain(unittest.TestCase):
         print("----------test_emails_x3_test_set----------")
         self.prepareVocab("train700.json")
         accuracy=evaluate("test300x3.json")
+        self.assertLessEqual(abs(self.initialAccuracyTest-accuracy),0.03)
+        
+    def test_duplication_words_train_set(self):
+        print("----------test_duplication_words_train_set----------")
+        self.prepareVocab("train_words.json")
+        accuracy=evaluate("test300.json")
+        self.assertLessEqual(abs(self.initialAccuracyTest-accuracy),0.03)
+
+    def test_duplication_words_test_set(self):
+        print("----------test_duplication_words_test_set----------")
+        self.prepareVocab("train700.json")
+        accuracy=evaluate("test_words.json")
         self.assertLessEqual(abs(self.initialAccuracyTest-accuracy),0.03)
 
 
